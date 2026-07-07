211b02bdf1bc8806584b746c55b9ebdbf16439ae
vrinda0706 <vrinda7605@gmail.com>
2026-07-07 10:59:33 +0530

    Added Subarray Sum Equals K solution

---
+++ lec-1/SubarraySumEqualsK.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class SubarraySumEqualsK  {
+
+    public static int subarraySumEqualsK(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            prefixSum += num;
+
+            if (map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
+            }
+
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int k = sc.nextInt();
+
+        System.out.println(subarraySumEqualsK(nums, k));
+
+        sc.close();
+    }
+}
